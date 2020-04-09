55d9b5aa841ffa8d5e6117c9f51eac99d419eb6a
zhaoxinsss <40855960+zhaoxinsss@users.noreply.github.com>
2020-04-09 08:14:53 +0800

    测试

---
+++ settings.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''测试项目配置文件
+'''
+#2019/03/28 QTAF自动生成
+
+import os
+
+PROJECT_NAME = "tech"
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_MODE = "standard"
+INSTALLED_APPS = []
